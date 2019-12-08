71b16ebba668dbabd6b5509fa84272ce03a70069
Vladivostok1 <git>
2019-12-08 22:50:32 +0800

    init shop

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Category from '../views/Category.vue'
+import Cart from '../views/Cart.vue'
+import Profile from '../views/Profile'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/category',
+    name: 'category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    fajhfhasfhasjfweansifhnwefeasfs
+  </div>
+</template>
+
+
